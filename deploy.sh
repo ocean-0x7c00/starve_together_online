2f018634c6af9c3bb030bfa4f63e50d9de6edf51
Ocean <ocean1793@163.com>
2020-12-03 22:23:05 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ home_path=/home/ocean
 config_path=/home/ocean/dstsave/dst/World1
 mod_path=/home/ocean/dstserver/mods
 git_path=starve_together_online
+master_mod_path=/home/ocean/dstsave/dst/World1/Master
+caves_mod_path=/home/ocean/dstsave/dst/World1/Caves
 
 #1更新代码
 cd $home_path/$git_path
@@ -10,13 +12,9 @@ echo `pwd`
 git pull
 echo '代码更新成功................'
 #2将两个文件放到指定位置
-
-
-
-#3.删除现有cluster
-
-
-#4.拷贝新配置
+echo `pwd`
+cp $git_path/dedicated_server_mods_setup.lua $mod_path
+cp $git_path/modoverrides.lua  $master_mod_path $caves_mod_path
 
 
 #5.停止现有服务
