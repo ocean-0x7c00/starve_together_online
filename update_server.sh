1e45dd94af43225cf4e2c658e22691f30d9bcaed
Ocean <ocean1793@163.com>
2020-12-17 20:51:23 +0800

    update version

--- update_server.sh
+++ update_server.sh
@@ -1,4 +1,10 @@
-#! /usr/bin/bash
+#!/bin/bash
 cd /home/ocean/steamcmd
 ./steamcmd.sh +login anonymous +force_install_dir /home/ocean/dstserver +app_update 343050 +quit
 echo 'update success.......'
+
+cd /home/ocean/dstserver/bin/
+nohup /home/ocean/dstserver/bin/master_start.sh > /home/ocean/log/master_start.log 2>&1& echo $! > /home/ocean/log/master_start.pid
+nohup /home/ocean/dstserver/bin/cave_start.sh > /home/ocean/log/cave_start.log 2>&1& echo $! > /home/ocean/log/cave_start.pid
+
+echo 'start success........'
